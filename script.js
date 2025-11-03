7297cbb1839a9e755d04148645e5774a075c5585
djenidisimple <djenidigauss@gmail.com>
2025-11-03 19:20:58 +0300

    new feature

--- script.js
+++ script.js
@@ -5,10 +5,29 @@
 let btnMenu = document.querySelector('.btn-menu');
 let btnClose = document.querySelector('.btn-close');
 let navMenu = document.querySelector('.nav-menu');
+let btnFeature = document.querySelector('.btn-feature');
+let btnCompany = document.querySelector('.btn-company');
+let dropDown = document.querySelector('.drop-down');
+let listMenu = document.querySelector('.list-menu');
+let items2 = document.querySelector('.items-2');
 
-btnMenu.addEventListener('click', () => {
-  navMenu.style.display = 'block';
+// btnMenu.addEventListener('click', () => {
+//   navMenu.style.display = 'block';
+// });
+
+dropDown.addEventListener('mouseover', () => {
+  listMenu.style.display = 'block';
 });
+dropDown.addEventListener('mouseout', () => {
+  listMenu.style.display = 'none';
+});
+
+// btnCompany.addEventListener('mouseover', () => {
+//   items2.style.display = 'block';
+// });
+// btnCompany.addEventListener('mouseout', () => {
+//   items2.style.display = 'none';
+// });
 
 btnClose.addEventListener('click', () => {
   navMenu.style.display = 'none';
